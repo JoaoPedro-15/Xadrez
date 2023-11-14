c9691c5bb32bdc411fc1fb48e0c97957589f6231
João Pedro <joao.goncalves@sptech.school>
2023-11-14 16:14:03 -0300

    Implementando Web Data Viz

--- Login/login.js
+++
@@ -1,6 +0,0 @@
-function login() {
-    div_msg.innerHTML = '';
-
-    var email = input_email.value
-    var senha = input_senha.value
-}
---
+++ site/public/js/home.js
@@ -0,0 +1,7 @@
+function tutorial(){
+    window.location.href = '../public/tutorial.html';
+}
+
+function quiz(){
+    window.location.href = '../public/quiz.html';
+}
